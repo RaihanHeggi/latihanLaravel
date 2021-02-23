41285c11e6b9bd59fd36156d93c78dcd40732c6a
RaihanHeggi <syaraihanheggi@gmail.com>
2021-02-23 14:41:13 +0700

    Latihan Laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,15 @@
 @section('title', 'Belajar Laravel')
 
 @section('container')
-<div class= "container">
-    <div class="row">
-        <div class="col-10">
-            <h1 class="mt-3">Hello, world!</h1>
-        </dix>
-    </div>   
-</div>
+<section>
+    <div class= "container">
+        <div class="row">
+            <div class="col-10">
+                <h1 class="mt-3">Hello Welcome to Simple Laravel Website</h1>
+                <a href="" type="button" class="portofolio-button">Portofolio</a>
+            </dix>
+        </div>   
+    </div>
+</section>
+
 @endsection
